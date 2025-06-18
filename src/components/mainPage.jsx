9921d7143e7fc8be5035725fc10f8a74e717d0ec
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-06-18 21:52:10 +0100

    header spacing

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -8,20 +8,26 @@ import FoodSection from "./foodSection";
 function MainPage() {
   return (
     <>
-      <div className="main-page">
-        <img src={BarImage} alt="Bar Front" className="full-height-image" />
-        <h1 className="main-heading">The Kings Arms</h1>
-        <ReviewCarousel />
-        <a
-          className="tripadvisor-link"
-          href="https://www.tripadvisor.co.uk/Restaurant_Review-g528791-d3683423-Reviews-The_Kings_Arms-Oldham_Greater_Manchester_England.html#REVIEWS"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Read Our Reviews
-        </a>
-        <div className="scroll-arrow">▼</div>
-      </div>
+  <div className="main-page">
+  <img src={BarImage} alt="Bar Front" className="full-height-image" />
+
+  <div className="main-content">
+    <h1 className="main-heading">The Kings Arms</h1>
+    <ReviewCarousel />
+  </div>
+
+  <a
+    className="tripadvisor-link"
+    href="https://www.tripadvisor.co.uk/Restaurant_Review-g528791-d3683423-Reviews-The_Kings_Arms-Oldham_Greater_Manchester_England.html#REVIEWS"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Read Our Reviews
+  </a>
+
+  <div className="scroll-arrow">▼</div>
+</div>
+
       <section>
         <FoodSection />
       </section>
