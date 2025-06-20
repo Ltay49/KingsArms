578118e25054343f1f80fecd07aad0795aba4c71
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-06-20 11:22:52 +0100

    new styling

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -8,35 +8,40 @@ import FoodSection from "./foodSection";
 function MainPage() {
   return (
     <>
-<div className="main-page">
-  <img src={BarImage} alt="Bar Front" className="full-height-image" />
+      <div className="main-page">
+        <img src={BarImage} alt="Bar Front" className="full-height-image" />
 
-  <div className="main-content">
-    <h1 className="main-heading">The Kings Arms</h1>
-    <ReviewCarousel />
+        <div className="main-content">
+        <h1 className="main-heading">
+  <span className="heading-top">THE</span>
+  <span className="location">Grains Bar, Oldham OL4 2JX</span><br />
+  <span className="heading-bottom">KINGS ARMS</span>
+</h1>
 
-    <a
-      className="tripadvisor-link"
-      href="https://www.tripadvisor.co.uk/Restaurant_Review-g528791-d3683423-Reviews-The_Kings_Arms-Oldham_Greater_Manchester_England.html#REVIEWS"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Read Our Reviews
-    </a>
+          <ReviewCarousel />
 
-    <div
-      className="scroll-down-btn"
-      onClick={() =>
-        window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })
-      }
-    >
-      <span>Scroll</span>
-      <div className="chevron"></div>
-    </div>
-  </div>
-</div>
+          <div className="links-wrapper">
+            <a
+              className="tripadvisor-link"
+              href="https://www.tripadvisor.co.uk/Restaurant_Review-g528791-d3683423-Reviews-The_Kings_Arms-Oldham_Greater_Manchester_England.html#REVIEWS"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read Our Reviews
+            </a>
 
-
+            <div
+              className="scroll-down-btn"
+              onClick={() =>
+                window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
+              }
+            >
+              <span>Scroll</span>
+              <div className="chevron"></div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <section>
         <FoodSection />
