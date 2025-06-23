ecc3a4ea70325e5ffca5f7c1c06340f941369057
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-06-23 11:56:47 +0100

    scrolling

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -8,7 +8,16 @@ import WhoWhatWhere from "./whoWhatWhere";
 
 function MainPage() {
   const nextSectionRef = useRef(null);
-
+  
+  React.useEffect(() => {
+    function setVh() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
   const handleScrollDown = () => {
     nextSectionRef.current?.scrollIntoView({ behavior: "smooth" });
   };
